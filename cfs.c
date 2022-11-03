22fd295a93cb0478320dbe404e7f0f50c1e9c77a
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-11-03 20:19:32 +0300

    Update cfs.c

--- cfs.c
+++ cfs.c
@@ -3,8 +3,8 @@
 
 #include "distribute.h"
 
-#define MIN_ARGS_C 16
-#define MIN_ARGS_F 7
+#define MIN_ARGS_C 15
+#define MIN_ARGS_F 6
 #define MIN_ARGS 2
 
 int main(int argc, char const *argv[])
@@ -17,16 +17,19 @@ int main(int argc, char const *argv[])
     else
     {
         int minPrio, maxPrio;
-        int avgPL, minPL, maxPL, distPL;
-        int avgIAT, minIAT, maxIAT, distIAT;
-        int rqLen;
+        int avgPL, minPL, maxPL;
+        char *distPL, *distIAT;
+        int avgIAT, minIAT, maxIAT;
+        int rqLen, allp, outmode;
+        char *outfile = NULL;
+        char *infile = NULL;
 
         char prog_mode[2];
         strcpy(prog_mode, argv[1]);
 
         if ( strcmp(prog_mode, "C") == 0)
         {
-            if ( argc != MIN_ARGS_C )
+            if ( argc < MIN_ARGS_C )
             {
                 fprintf(stderr, "Argument number is not sufficient");
                 exit(-1);
@@ -35,19 +38,31 @@ int main(int argc, char const *argv[])
             minPrio = atoi(argv[2]); maxPrio = atoi(argv[3]);
             distPL = argv[4]; avgPL = atoi(argv[5]); minPL = atoi(argv[6]); maxPL = atoi(argv[7]);
             distIAT = argv[8]; avgIAT = atoi(argv[9]); minIAT = atoi(argv[10]); maxIAT = atoi(argv[11]);
-            rqLen = atoi(argv[12]);
+            rqLen = atoi(argv[12]); allp = atoi(argv[13]); outmode = atoi(argv[14]);
 
-
+            if ( argc == MIN_ARGS_C + 1 )
+            {
+                outfile = argv[15];
+            }
 
         }
         else if ( strcmp(prog_mode, "F") == 0 )
         {
-            if ( argc != MIN_ARGS_F )
+            if ( argc < MIN_ARGS_F )
             {
                 fprintf(stderr, "Argument number is not sufficient");
                 exit(-1);
             }
-            // Take command line parameters and params in the file
+            // Take command line parameters
+            rqLen = atoi(argv[2]);
+            allp = atoi(argv[3]);
+            outmode = atoi(argv[4]);
+            infile = argv[5];
+
+            if ( argc == MIN_ARGS_F + 1 )
+            {
+                outfile = argv[6];
+            }
         }
 
         // Continue execution
