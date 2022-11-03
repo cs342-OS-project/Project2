c97354145e29fbfd7368d53fd0cbff279f516f22
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-11-03 19:13:20 +0300

    Create distribute.h

---
+++ distribute.h
@@ -0,0 +1,79 @@
+#ifndef DISTRIBUTE
+#define DISTRIBUTE
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Declarations
+int generate_priority(int minPrio, int maxPrio);
+
+int generate_process_length(char *distPL, int avgPL, int minPL, int maxPL);
+
+int generate_interarrival_time(char *distIAT, int avgIAT, int minIAT, int maxIAT);
+
+
+// Implementation
+int generate_priority(int minPrio, int maxPrio)
+{
+    int num = (rand() % (maxPrio - minPrio + 1)) + minPrio;
+    return num;
+}
+
+int generate_process_length(char *distPL, int avgPL, int minPL, int maxPL)
+{
+    if ( strcmp(distPL, "fixed") == 0 )
+    {
+        return avgPL;
+    }
+    else if ( strcmp(distPL, "uniform") == 0 )
+    {
+        int length = (rand() % (maxPL - minPL + 1)) + minPL;
+        return length;
+    }
+    else if ( strcmp(distPL, "exponential") == 0 )
+    {
+        double lambda = (double) 1 / avgPL;
+        double x;
+
+        do
+        {
+            int u = rand() % 2;
+            x = ((-1) * log(1 - (double) u)) / lambda;
+        } while ( minPL > x || x > maxPL );
+
+        return (int) x;
+    }
+
+    return -1;
+}
+
+int generate_interarrival_time(char *distIAT, int avgIAT, int minIAT, int maxIAT)
+{
+    if ( strcmp(distIAT, "fixed") == 0 )
+    {
+        return avgIAT;
+    }
+    else if ( strcmp(distIAT, "uniform") == 0 )
+    {
+        int length = (rand() % (maxIAT - minIAT + 1)) + minIAT;
+        return length;
+    }
+    else if ( strcmp(distIAT, "exponential") == 0 )
+    {
+        double lambda = (double) 1 / avgIAT;
+        double x;
+
+        do
+        {
+            int u = rand() % 2;
+            x = ((-1) * log(1 - (double) u)) / lambda;
+        } while ( minIAT > x || x > maxIAT );
+
+        return (int) x;
+    }
+
+    return -1;
+}
+
+#endif
